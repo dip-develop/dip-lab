190a12540d65a812ae6992af365254d79ec525ec
Dmytro Prylutskyi <dmytro.prylutskyi@outlook.com>
2026-05-16 13:42:44 +0200

    Fix

--- databases/mysql-init.sql
+++ databases/mysql-init.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE IF NOT EXISTS cloud;
 CREATE DATABASE IF NOT EXISTS ccnet_db;
-CREATE DATABASE IF NOT EXISTS seafile_ui_db;
+CREATE DATABASE IF NOT EXISTS seafile_db;
+CREATE DATABASE IF NOT EXISTS seahub_db;
 
 CREATE USER IF NOT EXISTS 'cloud'@'%' IDENTIFIED WITH caching_sha2_password BY 'Bestame9101';
 GRANT ALL PRIVILEGES ON cloud.* TO 'cloud'@'%';
 GRANT ALL PRIVILEGES ON ccnet_db.* TO 'cloud'@'%';
-GRANT ALL PRIVILEGES ON seafile_ui_db.* TO 'cloud'@'%';
+GRANT ALL PRIVILEGES ON seafile_db.* TO 'cloud'@'%';
+GRANT ALL PRIVILEGES ON seahub_db.* TO 'cloud'@'%';
 
 ALTER USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY 'Bestame9101';
 FLUSH PRIVILEGES;
